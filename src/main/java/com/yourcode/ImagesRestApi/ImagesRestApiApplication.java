972285ceea3da46c7eb63e76254d109f515a0997
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2022-10-02 11:10:00 -0500

    Inicializando proyecto

---
+++ src/main/java/com/yourcode/ImagesRestApi/ImagesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.yourcode.ImagesRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesRestApiApplication.class, args);
+	}
+
+}
