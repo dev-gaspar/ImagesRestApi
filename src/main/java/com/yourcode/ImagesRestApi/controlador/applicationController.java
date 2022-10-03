c02e88b0ccb30eaa875a959161720802eeceae83
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2022-10-03 07:41:51 -0500

    Controller and rest controller

--- src/main/java/com/yourcode/ImagesRestApi/controlador/applicationController.java
+++ src/main/java/com/yourcode/ImagesRestApi/controlador/applicationController.java
@@ -3,29 +3,34 @@ package com.yourcode.ImagesRestApi.controlador;
 import com.yourcode.ImagesRestApi.modelos.Imagen;
 import com.yourcode.ImagesRestApi.servicios.ImagenServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
-
-@RestController
-@RequestMapping("/api")
-public class Controlador {
+@Controller
+@RequestMapping("/application")
+public class applicationController {
     @Autowired
     ImagenServicio imagenServicio;
 
-    @GetMapping("/AllImages")
-    public List<Imagen> verImages() {
-        return imagenServicio.getAllImages();
+    @GetMapping("/cargar")
+    public String cargarImages(Model model) {
+        Imagen img = new Imagen();
+        model.addAttribute("img", img);
+        return "CargarImagen";
     }
 
     @PostMapping("/save")
-    public Imagen guardar(@RequestParam("file") MultipartFile imagen) {
+    public String guardar(@RequestParam("file") MultipartFile imagen, RedirectAttributes redirectAttributes) {
         Imagen nuevaImagen = new Imagen();
 
         if (!imagen.isEmpty()) {
@@ -46,11 +51,21 @@ public class Controlador {
             }
 
         }
-        return nuevaImagen;
+        return "redirect:/application/cargar";
+    }
+
+    @GetMapping("/AllImages")
+    public String verImages(Model model) {
+        List<Imagen> images=imagenServicio.getAllImages();
+        model.addAttribute("img",images);
+        return "MostrarImagen";
     }
 
     @GetMapping("/show/{id}")
-    public Imagen mostrarImagen(@PathVariable Integer id) {
-        return imagenServicio.getImageById(id);
+    public String mostrarImagen(@PathVariable Integer id, Model model) {
+        List<Imagen> images= new ArrayList<>();
+        images.add(imagenServicio.getImageById(id));
+        model.addAttribute("img",images);
+        return "MostrarImagen";
     }
 }
